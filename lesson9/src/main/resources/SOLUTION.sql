92b29e4691298b4386b7ad3c852ca4a54b3c36da
Dmitrii Ostapchuk <dmit.ostapchuk@gmail.com>
2024-05-12 23:21:56 +0500

    more and more corrections (forth try)

--- lesson9/src/main/resources/SOLUTION.sql
+++ lesson9/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
 select s.* from student s join (select student_id, avg(mark) as avg_mark from mark group by student_id) as t on t.student_id = s.id where t.avg_mark > 8;
 select s.id, s.name from student s join (select student_id, min(mark) as min_mark from mark group by student_id) as t on t.student_id = s.id where t.min_mark > 7;
-select s.id, s.name from student s join (select student_id, year(payment_date) as year, count(*) as payments from payment group by student_id, year) as t on t.student_id = s.id where t.year = 2019 and t.payments > 2;
+select s.id, s.name from student s join (select student_id, year(payment_date) as payment_year, count(*) as payments from payment group by student_id, payment_year) as t on t.student_id = s.id where t.payment_year = 2019 and t.payments > 2;
 
